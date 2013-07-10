7aca6bf444bd14d266a6765ab9b3732e86e3277a
ColeTownsend <cole@coletownsend.com>
2013-07-10 13:41:32 -0400

    post stuff

--- balzac/posts.php
+++ balzac/posts.php
@@ -10,8 +10,9 @@
                 <?php while(posts()): ?>
                   <li>
                     <div class="deets left-slide"><h1><a href="<?php echo article_url(); ?>"><?php echo article_title(); ?></a></h1>
-                    <em><p class="date"><time datetime="<?php echo date(DATE_W3C, article_time()); ?>"><?php echo relative_time(article_time()); ?></time></p></em></div>
-                    <p class=""><?php echo article_description(); ?></p>
+                        <em><p class="date"><time datetime="<?php echo date(DATE_W3C, article_time()); ?>"><?php echo relative_time(article_time()); ?></time></p></em>
+                        <p class=""><?php echo article_description(); ?></p>
+                    </div>
                   </li>
                 <?php endwhile; ?>
               </ol>
