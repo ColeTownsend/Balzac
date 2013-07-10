a25d6fcf80356a45f3a620f626b85e3be24ac6b9
ColeTownsend <cole@coletownsend.com>
2013-07-10 12:51:49 -0400

    what even.
    crushing it.

--- balzac/posts.php
+++ balzac/posts.php
@@ -11,6 +11,7 @@
                   <li>
                     <div class="deets left-slide"><h1><a href="<?php echo article_url(); ?>"><?php echo article_title(); ?></a></h1>
                     <em><p class="date"><time datetime="<?php echo date(DATE_W3C, article_time()); ?>"><?php echo relative_time(article_time()); ?></time></p></em></div>
+                    <p class=""><?php echo article_description(); ?></p>
                   </li>
                 <?php endwhile; ?>
               </ol>
