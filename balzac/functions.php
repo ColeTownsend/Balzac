040243a752cd8dd865befc8792ba97b49d06906d
ColeTownsend <cole@coletownsend.com>
2013-07-10 12:59:57 -0400

    sweet

--- balzac/functions.php
+++ balzac/functions.php
@@ -1,64 +1,119 @@
 <?php
 
 /*
-	Custom theme functions
+  Custom theme functions
 
-	Note: we recommend you prefix all your functions to avoid any naming
-	collisions or wrap your functions with if function_exists braces.
+  Note: we recommend you prefix all your functions to avoid any naming
+  collisions or wrap your functions with if function_exists braces.
 */
 function numeral($number) {
-	$test = abs($number) % 10;
-	$ext = ((abs($number) % 100 < 21 and abs($number) % 100 > 4) ? 'th' : (($test < 4) ? ($test < 3) ? ($test < 2) ? ($test < 1) ? 'th' : 'st' : 'nd' : 'rd' : 'th'));
-	return $number . $ext;
+  $test = abs($number) % 10;
+  $ext = ((abs($number) % 100 < 21 and abs($number) % 100 > 4) ? 'th' : (($test < 4) ? ($test < 3) ? ($test < 2) ? ($test < 1) ? 'th' : 'st' : 'nd' : 'rd' : 'th'));
+  return $number . $ext;
 }
 
 function count_words($str) {
-	return count(preg_split('/\s+/', strip_tags($str), null, PREG_SPLIT_NO_EMPTY));
+  return count(preg_split('/\s+/', strip_tags($str), null, PREG_SPLIT_NO_EMPTY));
 }
 
 function pluralise($amount, $str, $alt = '') {
-	return intval($amount) === 1 ? $str : $str . ($alt !== '' ? $alt : 's');
+  return intval($amount) === 1 ? $str : $str . ($alt !== '' ? $alt : 's');
 }
 
-function relative_time($date) {
-	if(is_numeric($date)) $date = '@' . $date;
+function relative_time($time) {
+  // make sure $date is a time stamp
+  if(!is_numeric($time)) $time = strtotime($time);
 
-	$user_timezone = new DateTimeZone(Config::app('timezone'));
-	$date = new DateTime($date, $user_timezone);
+  $elapsed = time() - $time;
 
-	// get current date in user timezone
-	$now = new DateTime('now', $user_timezone);
+  if($elapsed <= 1) {
+    return 'Just now';
+  }
 
-	$elapsed = $now->format('U') - $date->format('U');
+  $times = array(
+    31104000 => 'year',
+    2592000 => 'month',
+    604800 => 'week',
+    86400 => 'day',
+    3600 => 'hour',
+    60 => 'minute',
+    1 => 'second'
+  );
 
-	if($elapsed <= 1) {
-		return 'Just now';
-	}
+  foreach($times as $seconds => $title) {
+    $rounded = $elapsed / $seconds;
 
-	$times = array(
-		31104000 => 'year',
-		2592000 => 'month',
-		604800 => 'week',
-		86400 => 'day',
-		3600 => 'hour',
-		60 => 'minute',
-		1 => 'second'
-	);
+    if($rounded > 1) {
+      $rounded = round($rounded);
+      return $rounded . ' ' . pluralise($rounded, $title) . ' ago';
+    }
+  }
+}
 
-	foreach($times as $seconds => $title) {
-		$rounded = $elapsed / $seconds;
+function hide_show_menu_item($id, $key, $default = '')
+{
+  if($extend = Extend::field('page', $key, $id)) {
+      return Extend::value($extend, $default);
+  }
+  return $default;
+}
 
-		if($rounded > 1) {
-			$rounded = round($rounded);
-			return $rounded . ' ' . pluralise($rounded, $title) . ' ago';
-		}
+function rwar_latest_posts($limit = 3) {
+    // only run on the first call
+    if( ! Registry::has('rwar_latest_posts')) {
+        // capture original article if one is set
+        if($article = Registry::get('article')) {
+            Registry::set('original_article', $article);
+        }
+    }
+
+    if( ! $posts = Registry::get('rwar_latest_posts')) {
+        $posts = Post::where('status', '=', 'published')->sort('created', 'desc')->take($limit)->get();
+
+        Registry::set('rwar_latest_posts', $posts = new Items($posts));
+    }
+
+    if($result = $posts->valid()) {
+        // register single post
+        Registry::set('article', $posts->current());
+
+        // move to next
+        $posts->next();
+    }
+    else {
+        // back to the start
+        $posts->rewind();
+
+        // reset original article
+        Registry::set('article', Registry::get('original_article'));
+
+        // remove items
+        Registry::set('rwar_latest_posts', false);
+    }
+
+    return $result;
+}
+
+function mytheme_latest_post() {
+	$post = Post::where('status', '=', 'published')->sort('created', 'desc')->take(1)->fetch();
+		
+	if($post) {
+		// set globally to use article theming functions
+		Registry::set('article', $post);
+
+		// or you can just return and use the raw object
+		return $post;
 	}
 }
 
+
+/*
+  Twitter
+*/
 function twitter_account() {
-	return site_meta('twitter', 'idiot');
+  return Config::get('meta.twitter');
 }
 
 function twitter_url() {
-	return 'https://twitter.com/' . twitter_account();
+  return 'http://twitter.com/' . twitter_account();
 }
---
+++ balzac/page-portfolio.php
@@ -0,0 +1,71 @@
+<?php
+
+  $allowCat = array(3);
+
+  function article_category_id() {
+    if($category = Registry::prop('article', 'category')) {
+      $categories = Registry::get('all_categories');
+      return $categories[$category]->id;
+    }
+  }
+    
+    function portf_list() {
+      // only run on the first call
+      if( ! Registry::has('rwar_post_archive')) {
+        // capture original article if one is set
+        if($article = Registry::get('article')) {
+          Registry::set('original_article', $article);
+        }
+      }
+    
+      if( ! $posts = Registry::get('rwar_post_archive')) {
+        $posts = Post::where('status', '=', 'published')->sort('created', 'desc')->get();
+    
+        Registry::set('rwar_post_archive', $posts = new Items($posts));
+      }
+    
+      if($result = $posts->valid()) {
+        // register single post
+        Registry::set('article', $posts->current());
+    
+        // move to next
+        $posts->next();
+      }
+      else {
+        // back to the start
+        $posts->rewind();
+    
+        // reset original article
+        Registry::set('article', Registry::get('original_article'));
+    
+        // remove items
+        Registry::set('rwar_post_archive', false);
+      }
+    
+      return $result;
+    }
+
+?>
+
+<?php theme_include('header'); ?>
+    <div id='bump'>
+        <section class="article archive">
+          <article class="archive-wrap">
+              <ol class="post-list">
+                 <lh style="text-align: center;"><h1><span class="bb">Recent Posts</span></h1></lh>
+        <?php while(portf_list()): ?>
+            <?php foreach($allowCat as $cat): ?>
+                <?php if(article_category_id() == $cat): ?>
+                  <li>
+                    <div class="deets left-slide"><h1><a href="<?php echo article_url(); ?>"><?php echo article_title(); ?></a></h1>
+                    <em><p class="date"><time datetime="<?php echo date(DATE_W3C, article_time()); ?>"><?php echo relative_time(article_time()); ?></time></p></em></div>
+                    <p class=""><?php echo article_description(); ?></p>
+                  </li>
+                <?php endif; ?>
+            <?php endforeach; ?>
+        <?php endwhile; ?>
+              </ol>
+          </article>
+        </section>
+    </div>
+<?php theme_include('footer'); ?>
