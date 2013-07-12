e8cedc743f3ba90006bdefbd7a298f6df5a37853
ColeTownsend <cole@coletownsend.com>
2013-07-12 17:06:05 -0400

    Fixies.

--- balzac/posts.php
+++ balzac/posts.php
@@ -6,7 +6,7 @@
               <!-- We have posts, it's safe to loop. -->
               <h1></h1>
               <ol class="post-list">
-                 <lh style="text-align: center;"><h1><span class="bb">Recent Posts</span></h1></lh>
+                 <lh><h1><span class="bb">Recent Posts</span></h1></lh>
                 <?php while(posts()): ?>
                   <li>
                     <div class="deets left-slide"><h1><a href="<?php echo article_url(); ?>"><?php echo article_title(); ?></a></h1>
