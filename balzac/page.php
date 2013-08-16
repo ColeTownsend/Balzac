d4dfdd99bb93920ae688f5f9bbb45defba52c1de
Benjamin Dengler <benjamin.dengler@gmail.com>
2013-08-16 14:08:51 +0200

    Added featured-image variable for pages

--- balzac/page.php
+++ balzac/page.php
@@ -2,7 +2,7 @@
 
     <section class="article" id="article-<?php echo article_id(); ?>">
       <div class="overlay"></div>
-      <div class="featured-image" style="background-image: url(<?php echo article_custom_field('featured-image', '/themes/balzac/img/soft-trees.jpg'); ?>);"></div>
+      <div class="featured-image" style="background-image: url(<?php echo page_custom_field('featured-image', '/themes/balzac/img/soft-trees.jpg'); ?>);"></div>
 
       <article class="wrap post">
         <header class="post-header">
