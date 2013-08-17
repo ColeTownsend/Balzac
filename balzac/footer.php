bdc9199b2e738cc70bb9d475704be954dd428ab2
ColeTownsend <cole@coletownsend.com>
2013-08-17 15:12:28 -0400

    add backslash

--- balzac/footer.php
+++ balzac/footer.php
@@ -29,7 +29,7 @@
 
   <!-- If they're out, get some from the cellar -->
   <script>window.jQuery || document.write('<script src="<?php echo theme_url('/js/jquery.min.js'); ?>"><\/script>')</script>
-  <script src="<?php echo theme_url('js/retina.min.js'); ?>"></script>
+  <script src="<?php echo theme_url('/js/retina.min.js'); ?>"></script>
 
   <!-- Custom JS -->
   <script src="<?php echo theme_url('/js/scripts.js'); ?>"></script>
